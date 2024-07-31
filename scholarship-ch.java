6819e0154ccad48cd3f87f1329408e12cdf9a0fc
Apple Donato <apple.donato0111@gmail.com>
2024-07-31 19:56:41 +0800

    Create scholarship-ch.java

---
+++ scholarship-ch.java
@@ -0,0 +1,382 @@
+import java.util.Scanner;
+public class Labif5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter grade in Subject 1:\t");
+		float sub1 = sc.nextFloat(); 
+		if (sub1>=97.5 && sub1<=100) {
+			System.out.println("Subject 1 = " + sub1); 
+		 if (sub1>100) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+			
+		
+			
+		System.out.println("Please enter grade in Subject 2:\t");
+		float sub2 = sc.nextFloat();
+		if (sub2>=97.5 && sub2<=100) {
+			System.out.println("Subject 2 = " + sub2); 
+		}if (sub2>100) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 3:\t");
+		float sub3 = sc.nextFloat();
+		if (sub3>=97.5 && sub3<=100) {
+			System.out.println("Subject 3 = " + sub3); 
+		}if (sub3>100) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 4:\t");
+		float sub4 = sc.nextFloat();
+		if (sub4>=97.5 && sub4<=100) {
+			System.out.println("Subject 4 = " + sub4); 
+		}if (sub4>100) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 5:\t");
+		float sub5 = sc.nextFloat();
+		if (sub5>=97.5 && sub5<=100) {
+			System.out.println("Subject 5 = " + sub5); 
+		}if (sub5>100) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 6:\t");
+		float sub6 = sc.nextFloat();
+		if (sub6>=97.5 && sub6<=100) {
+			System.out.println("Subject 6 = " + sub6); 
+		}if (sub6>100) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 7:\t");
+		float sub7 = sc.nextFloat();
+		if (sub7>=97.5 && sub7<=100) {
+			System.out.println("Subject 7 = " + sub7); 
+		}if (sub7>100) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 8:\t");
+		float sub8 = sc.nextFloat();
+		if (sub8>=97.5 && sub8<=100) {
+			System.out.println("Subject 8 = " + sub8); 
+		}if (sub8>100) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 9:\t");
+		float sub9 = sc.nextFloat();
+		if (sub9>=97.5 && sub9<=100) {
+			System.out.println("Subject 9 = " + sub9); 
+		}if (sub9>100) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 10:\t");
+		float sub10 = sc.nextFloat();
+		if (sub10>=97.5 && sub10<=100) {
+			System.out.println("Subject 10 = " + sub10); 
+		}if (sub10>100) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+			float average = (sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7 + sub8 + sub9 + sub10)/10;
+			String averagetotal = "Average :" + " " + average;
+			System.out.println( averagetotal + "Scholarship Grant: President's List\n" + "Discount: 100%\n");
+				//What to improve: if one enters numbers greater than 100, it should not proceed to the next but instead tells the user to re-enter it to the right value
+				//yes switch cases are needed to execute that kind of problem, but..could we do it if an If-Else statement?
+				//(UPDATED: FINALLY RESOLVED)the loop must end and start again and so we use the return statement
+				//Another problem: i want to continue the loop yet it seems there is a problem after else if on sub 2, how do i resolve that?
+	}else {
+		System.out.println("We're sorry, you are not eligible for this Scholarship. " +
+		   "However, we have discounts available for those who obtained grades ranging from 97-95");
+	}
+		
+		
+		System.out.println("Discount A:\t");
+		System.out.println("Please enter grade in Subject 1:\t");
+		float subj1 = sc.nextFloat(); 
+		if (subj1>=94.5 && subj1<=97) {
+			System.out.println("Subject 1 = " + subj1); 
+		 if (subj1>97) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+			
+		
+			
+		System.out.println("Please enter grade in Subject 2:\t");
+		float subj2 = sc.nextFloat();
+		if (subj2>=94.5 && subj2<=97) {
+			System.out.println("Subject 2 = " + subj2); 
+		}if (subj2>97) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 3:\t");
+		float subj3 = sc.nextFloat();
+		if (subj3>=94.5 && subj3<=97) {
+			System.out.println("Subject 3 = " + subj3); 
+		}if (subj3>97) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 4:\t");
+		float subj4 = sc.nextFloat();
+		if (subj4>=94.5 && subj4<=97) {
+			System.out.println("Subject 4 = " + subj4); 
+		}if (subj4>97) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 5:\t");
+		float subj5 = sc.nextFloat();
+		if (subj5>=94.5 && subj5<=97) {
+			System.out.println("Subject 5 = " + subj5); 
+		}if (subj5>97) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 6:\t");
+		float subj6 = sc.nextFloat();
+		if (subj6>=94.5 && subj6<=97) {
+			System.out.println("Subject 6 = " + subj6); 
+		}if (subj6>97) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 7:\t");
+		float subj7 = sc.nextFloat();
+		if (subj7>=94.5 && subj7<=97) {
+			System.out.println("Subject 7 = " + subj7); 
+		}if (subj7>97) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 8:\t");
+		float subj8 = sc.nextFloat();
+		if (subj8>=94.5 && subj8<=97) {
+			System.out.println("Subject 8 = " + subj8); 
+		}if (subj8>97) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 9:\t");
+		float subj9 = sc.nextFloat();
+		if (subj9>=94.5 && subj9<=97) {
+			System.out.println("Subject 9 = " + subj9); 
+		}if (subj9>97) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 10:\t");
+		float subj10 = sc.nextFloat();
+		if (subj10>=94.5 && subj10<=97) {
+			System.out.println("Subject 10 = " + subj10); 
+		}if (subj10>97) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+			float average = (subj1 + subj2 + subj3 + subj4 + subj5 + subj6 + subj7 + subj8 + subj9 + subj10)/10;
+			String averagetotal = "Average :" + " " + average;
+			System.out.println( averagetotal  + "\n" + "Discount: 75%\n");
+		
+		}else {
+			System.out.println("We're sorry, you are not eligible for this Discount." +
+					   "However, we have another discount available for those who obtained grades ranging from 94-90");
+				}
+		
+		System.out.println("Discount B:\t");
+		System.out.println("Please enter grade in Subject 1:\t");
+		float subject1 = sc.nextFloat(); 
+		if (subject1>=89.5 && subject1<=94) {
+			System.out.println("Subject 1 = " + subject1); 
+		 if (subject1>97) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+			
+		
+			
+		System.out.println("Please enter grade in Subject 2:\t");
+		float subject2 = sc.nextFloat();
+		if (subject2>=89.5 && subject2<=94) {
+			System.out.println("Subject 2 = " + subject2); 
+		}if (subject2>94) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 3:\t");
+		float subject3 = sc.nextFloat();
+		if (subject3>=89.5 && subject3<=94) {
+			System.out.println("Subject 3 = " + subject3); 
+		}if (subject3>94) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 4:\t");
+		float subject4 = sc.nextFloat();
+		if (subject4>=89.5 && subject4<=94) {
+			System.out.println("Subject 4 = " + subject4); 
+		}if (subject4>94) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 5:\t");
+		float subject5 = sc.nextFloat();
+		if (subject5>=89.5 && subject5<=94) {
+			System.out.println("Subject 5 = " + subject5); 
+		}if (subject5>94) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 6:\t");
+		float subject6 = sc.nextFloat();
+		if (subject6>=89.5 && subject6<=94) {
+			System.out.println("Subject 6 = " + subject6); 
+		}if (subject6>94) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		System.out.println("Please enter grade in Subject 7:\t");
+		float subject7 = sc.nextFloat();
+		if (subject7>=89.5 && subject7<=94) {
+			System.out.println("Subject 7 = " + subject7); 
+		}if (subject7>94) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 8:\t");
+		float subject8 = sc.nextFloat();
+		if (subject8>=89.5 && subject8<=94) {
+			System.out.println("Subject 8 = " + subject8); 
+		}if (subject8>94) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 9:\t");
+		float subject9 = sc.nextFloat();
+		if (subject9>=89.5 && subject9<=94) {
+			System.out.println("Subject 9 = " + subject9); 
+		}if (subject9>94) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+		
+		
+		
+		
+		System.out.println("Please enter grade in Subject 10:\t");
+		float subject10 = sc.nextFloat();
+		if (subject10>=89.5 && subject10<=94) {
+			System.out.println("Subject 10 = " + subject10); 
+		}if (subject10>94) {
+			System.out.println("Invalid Grade, Please try again");
+			return;
+		}
+			float average = (subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7 + subject8 + subject9 + subject10)/10;
+			String averagetotal = "Average :" + " " + average;
+			System.out.println( averagetotal  + "\n" + "Discount: 50%\n");
+		
+		}else {
+			System.out.println("We're sorry, you are not eligible for this Discount.");
+				}
+			}
+		 }
+		
+		
+			
+			
+	
+	
+		
+			
+
+		
+		
+
